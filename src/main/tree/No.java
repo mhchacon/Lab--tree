1f6aa4959e59b8734fa702887c62b4aca2fdfa27
mhchacon <mariana.chacon@ufrpe.br>
2024-09-05 15:16:31 -0300

    first

---
+++ src/main/tree/No.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class No {
+    int valor; 
+    No esquerda; 
+    No direita; 
+  
+    
+    No(int valor) {
+      this.valor = valor;
+      esquerda = null;
+      direita = null;
+    }
+}
